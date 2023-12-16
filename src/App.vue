6192c5fb71f063a4c3632f055a0f76da42345f7b
root <root@SNKNBA>
2023-12-16 15:06:42 -0300

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <el-menu
+      mode="horizontal"
+      background-color="#3498db"
+      text-color="#ffffff"
+      active-text-color="#ffffff"
+    >
+      <el-menu-item index="1">
+        <router-link :to="{ path: '/'}" class="custom-menu-item">Home</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link :to="{ path: '/todo-list'}" class="custom-menu-item">Todo List</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link :to="{ path: '/issue-list'}" class="custom-menu-item">Issues</router-link>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <router-link :to="{ path: '/todo-issues'}" class="custom-menu-item">Todo Issues</router-link>
+      </el-menu-item>
+    </el-menu>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
